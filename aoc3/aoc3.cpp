1f8c3707088e5f65c29e50915fa259e0115d70ee
Paul Ranson <paul@epicyclism.com>
2022-12-03 09:06:20 +0000

    Day 3

---
+++ aoc3/aoc3.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+auto get_input()
+{
+    std::vector<std::string> rv;
+    std::string ln;
+    while(std::getline(std::cin, ln))
+        rv.emplace_back(ln);
+    return rv;
+}
+
+int val(char c)
+{
+    return (c >= 'a') ? c - ('a' - 1) : c - ('A' - 1) + 26;
+}
+
+auto pt1(auto const& in)
+{
+    int sum { 0 };
+    std::string tmp { ' ', 1};
+    for(auto& s: in)
+    {
+        auto fs { s.substr(0, s.size()/2) };
+        auto ss { s.substr(s.size()/2) };
+        std::ranges::sort(fs);
+        std::ranges::sort(ss);
+        std::ranges::set_intersection(fs, ss, tmp.begin());
+        sum += val(tmp.front());
+    }
+    return sum;
+}
+
+auto pt2(auto const& in)
+{
+    std::string tmp;
+    std::string tmp2;
+    int sum{0};
+    auto it = in.begin();
+    while(it != in.end())
+    {
+        auto s1 {*it++};
+        auto s2 {*it++};
+        auto s3{*it++};
+        std::ranges::sort(s1);
+        std::ranges::sort(s2);
+        std::ranges::sort(s3);
+        tmp.clear();
+        tmp2.clear();
+        std::ranges::set_intersection(s1, s2, std::back_inserter(tmp));
+        std::ranges::set_intersection(tmp, s3, std::back_inserter(tmp2));
+        sum += val(tmp2.front());
+    }
+    return sum;
+}
+
+int main()
+{
+    auto in {get_input()};
+
+    std::cout << "pt1 = " << pt1(in) << "\n";
+    std::cout << "pt2 = " << pt2(in) << "\n";
+}
