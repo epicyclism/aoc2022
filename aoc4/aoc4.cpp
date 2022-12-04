1d0ef0479aa4e00860e70a795b55afca37eb46b3
Paul Ranson <paul@epicyclism.com>
2022-12-04 09:07:13 +0000

    day4

--- aoc4/aoc4.cpp
+++ aoc4/aoc4.cpp
@@ -3,29 +3,45 @@
 #include <vector>
 #include <algorithm>
 
+#include <ctre_inc.h>
+
+struct ft_t
+{
+    int f1_;
+    int t1_;
+    int f2_;
+    int t2_;
+};
+
 auto get_input()
 {
-    std::vector<std::string> rv;
+    std::vector<ft_t> rv;
+    constexpr auto rx = ctll::fixed_string{ R"((\d+)-(\d+),(\d+)-(\d+))" };
     std::string ln;
     while(std::getline(std::cin, ln))
-        rv.emplace_back(ln);
+    {
+        auto[m, f1, t1, f2, t2] = ctre::match<rx>(ln);
+        rv.emplace_back(sv_to_t<int>(f1.to_view()), sv_to_t<int>(t1.to_view()), sv_to_t<int>(f2.to_view()), sv_to_t<int>(t2.to_view()));
+    }
     return rv;
 }
 
-auto pt1(auto const& in)
+bool contains(ft_t r)
 {
-    return in.size();
+    return r.f1_<= r.f2_ && r.t1_ >= r.t2_ ||
+        r.f2_<= r.f1_ && r.t2_ >= r.t1_ ;
 }
 
-auto pt2(auto const& in)
+bool overlaps(ft_t r)
 {
-    return in.size();
+    return r.t1_>= r.t2_ && r.f1_ <= r.t2_ ||
+        r.t2_ >= r.f1_ && r.f2_ <= r.t1_ ;
 }
 
 int main()
 {
     auto in {get_input()};
 
-    std::cout << "pt1 = " << pt1(in) << "\n";
-    std::cout << "pt2 = " << pt2(in) << "\n";
+    std::cout << "pt1 = " << std::ranges::count_if(in, [](auto& r){return contains(r);}) << "\n";
+    std::cout << "pt2 = " << std::ranges::count_if(in, [](auto& r){return overlaps(r);}) << "\n";
 }
