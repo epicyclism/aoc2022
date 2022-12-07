3afb6990f97f1499b5bd171d56608f65385f8687
Paul Ranson <paul@epicyclism.com>
2022-12-07 09:36:32 +0000

    aoc7 tool for ever to notice that directory names were repeated in different sub directories...

--- aoc7/aoc7.cpp
+++ aoc7/aoc7.cpp
@@ -2,28 +2,73 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <numeric>
+#include <map>
 
 #include <ctre_inc.h>
 
-auto get_input()
+// durectory to total size contained
+using ds_t = std::map<std::string, uint64_t>;
+
+uint64_t enumerate_dir(std::string const& dir, ds_t& d)
 {
-    std::vector<std::string> in;
+    std::string cwd { dir };
     std::string ln;
     while(std::getline(std::cin, ln))
     {
-        in.emplace_back(std::move(ln));
+        if(auto[m, s, n] = ctre::match<R"((\d+) (\H+))">(ln); m) // file listing
+        {
+            d[dir] += sv_to_t<uint64_t>(s);
+        }
+        else
+        if(auto[m2, n2] = ctre::match<R"(\$ cd (\H+))">(ln); m2) // change dir
+        {
+            auto arg {n2.view()};
+            if(arg == "..")
+                return d[dir];
+            else
+                cwd += arg;
+        }
+        else
+        if(ctre::match<R"(\$ ls)">(ln)) // list
+        {
+            auto dd = enumerate_dir(cwd, d);
+            d[dir] += dd;
+        }
+        else
+        if(ctre::match<R"(dir (\H+))">(ln))
+        {
+        }
+        else
+            std::cout << "no match - " << ln << "\n";
     }
+    return d[dir] ;
+}
+
+auto get_input()
+{
+    ds_t in;
+    enumerate_dir("", in);
     return in;
 }
 
 auto pt1(auto const& in)
 {
-    return in.size();
+    return std::accumulate(in.begin(), in.end(), 0ULL, [](auto s, auto& r){if(r.second <= 100000) return s + r.second ; else return s;});
 }
 
 auto pt2(auto const& in)
 {
-    return in.size();
+    constexpr auto max      { 70000000ULL };
+    constexpr auto free_min { 30000000ULL };
+    auto cur {in.at("/")};
+    auto max_min { free_min - (max - cur)};
+    auto dir_sz{cur};
+    for(auto& d : in)
+        if( d.second > max_min && dir_sz > d.second)
+            dir_sz = d.second;
+
+    return dir_sz;
 }
 
 int main()
