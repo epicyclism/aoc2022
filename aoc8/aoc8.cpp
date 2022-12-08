9239a7309b103cd8538475de5e3818061a021709
epicyclism <paul@epicyclism.com>
2022-12-08 18:12:02 +0000

    Condense parts 1 and 2

--- aoc8/aoc8.cpp
+++ aoc8/aoc8.cpp
@@ -19,7 +19,7 @@ auto get_input()
     return std::pair(data, stride);
 }
 
-template<typename F> void proc_1(auto& mds, F f)
+template<typename F> void proc_1(auto const& mds, F f)
 {
     char max_so_far { -1 };
     for (int i = 0; i < mds.extent(0); ++i)
@@ -39,29 +39,10 @@ template<typename F> void proc_1(auto& mds, F f)
     }
 }
 
-auto pt1(auto in)
+template<typename F> void proc_2(auto const& mds, F f)
 {
-    stdex::mdspan md(in.first.data(), in.second, in.second);
-    std::vector<bool> ss(in.second * in.second);
-    for (int r = 0; r < md.extent(0); ++r)
-    {
-        auto mds = stdex::submdspan(md, r, stdex::full_extent);
-        proc_1(mds, [&](auto c) {ss[r*in.second+c] = true; });
-    }
-    for (int c = 0; c < md.extent(1); ++c)
-    {
-        auto mds = stdex::submdspan(md, stdex::full_extent, c);
-        proc_1(mds, [&](auto r) {ss[r * in.second + c] = true; });
-    }
-    return std::ranges::count(ss, true);
-}
-
-template<typename F> void proc_2(auto& mds, F f)
-{
-    // for each tree with a view
     for (int t = 1; t < mds.extent(0); ++t)
     {
-        // look 'left'
         int v{ 0 };
         auto h{ mds[t] };
         for (auto c = t - 1; c >= 0; --c)
@@ -72,7 +53,6 @@ template<typename F> void proc_2(auto& mds, F f)
         }
         f(t, v);
         v = 0;
-        // look 'right'
         for (auto c = t + 1; c < mds.extent(0); ++c)
         {
             ++v;
@@ -83,28 +63,31 @@ template<typename F> void proc_2(auto& mds, F f)
     }
 }
 
-auto pt2(auto const& in)
+auto pt12(auto const& in)
 {
     stdex::mdspan md(in.first.data(), in.second, in.second);
-    std::vector<int> out(in.first.size(), 1);
+    std::vector<bool> ss(in.second * in.second); // record out for part 1!
+    std::vector<int> out(in.first.size(), 1);    // record out for part 2!
     stdex::mdspan mdo(out.data(), in.second, in.second);
-    for (int r = 1; r < md.extent(0) - 1; ++r)
+    for (int r = 0; r < md.extent(0); ++r)
     {
         auto mds = stdex::submdspan(md, r, stdex::full_extent);
+        proc_1(mds, [&](auto c) {ss[r*in.second+c] = true; });
         proc_2(mds, [&mdo, r](auto c, auto v) {mdo(c, r) *= v; });
     }
-    for (int c = 1; c < md.extent(1) - 1; ++c)
+    for (int c = 0; c < md.extent(1); ++c)
     {
         auto mds = stdex::submdspan(md, stdex::full_extent, c);
+        proc_1(mds, [&](auto r) {ss[r * in.second + c] = true; });
         proc_2(mds, [&mdo, c](auto r, auto v) {mdo(c, r) *= v; });
     }
-    return *std::ranges::max_element(out);
+    return std::pair(std::ranges::count(ss, true), *std::ranges::max_element(out));
 }
 
 int main()
 {
     auto in {get_input()};
-
-    std::cout << "pt1 = " << pt1(in) << "\n";
-    std::cout << "pt2 = " << pt2(in) << "\n";
+    auto p12{ pt12(in) };
+    std::cout << "pt1 = " << p12.first << "\n";
+    std::cout << "pt2 = " << p12.second << "\n";
 }
