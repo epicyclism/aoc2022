29a5841062f5c9daab375c54571874d528b0d32c
epicyclism <paul@epicyclism.com>
2022-12-09 13:50:00 +0000

    Interim aoc9 pt2

--- aoc9/aoc9.cpp
+++ aoc9/aoc9.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iterator>
 #include <string>
 #include <vector>
 #include <array>
@@ -15,24 +16,30 @@ auto get_input()
     {
         auto[m, c, s] = ctre::match<R"(([DLRU]) (\d+))">(ln);
         auto d { sv_to_t<short>(s)};
+        pt_t step;
         switch(c.view()[0])
         {
             case 'D':
-                in.emplace_back(0, -d);
+                step = { 0, -1 };
+                //in.emplace_back(0, -d);
                 break;
             case 'L':
-                in.emplace_back(-d, 0);
+                step = { -1, 0 };
+                //in.emplace_back(-d, 0);
                 break;
             case 'R':
-                in.emplace_back(d, 0);
+                step = { 1, 0 };
+                //in.emplace_back(d, 0);
                 break;
             case 'U':
-                in.emplace_back(0, d);
+                step = { 0, 1 };
+                //in.emplace_back(0, d);
                 break;
             default:
                 break; // NOT REACHED
         }
-        ;
+        for (int n = 0; n < d; ++n)
+            in.push_back(step);
     }
     return in;
 }
@@ -94,14 +101,40 @@ pt_t simple_follow(pt_t H, pt_t T, auto& sT, bool record)
     return T;
 }
 
+void check0(pt_t F, pt_t T)
+{
+    if (std::abs(F.first - T.first) > 1 || std::abs(F.second - T.second) > 1)
+    {
+        std::cout << "BOGUS0 step, (" << F.first << ", " << F.second << ") to (" << T.first << ", " << T.second << ")\n";
+    }
+}
+void check1(pt_t F, pt_t T)
+{
+    if (std::abs(F.first - T.first) > 1 || std::abs(F.second - T.second) > 1)
+    {
+        std::cout << "BOGUS1 step, (" << F.first << ", " << F.second << ") to (" << T.first << ", " << T.second << ")\n";
+    }
+}
+void check2(pt_t F, pt_t T)
+{
+    if (std::abs(F.first - T.first) > 1 || std::abs(F.second - T.second) > 1)
+    {
+        std::cout << "BOGUS2 step, (" << F.first << ", " << F.second << ") to (" << T.first << ", " << T.second << ")\n";
+    }
+}
+
 pt_t follow(pt_t H, pt_t T, auto& sT, bool record)
 {
     // tail has to move?
     auto d = distance(H, T);
 //    std::cout << "d = " << d << "\n";
-    if(d < 2)
+    if (d < 2)
+    {
+        check0(H, T);
         return T;
-    if( !(H.first == T.first || H.second == T.second))
+    }
+    auto T0{ T };
+    if (!(H.first == T.first || H.second == T.second))
     {
         if(std::abs(H.first - T.first) == 1)
         {
@@ -122,7 +155,9 @@ pt_t follow(pt_t H, pt_t T, auto& sT, bool record)
         if(record)
             sT.insert(T);
     }
-    return simple_follow(H, T, sT, record);
+    auto rT = simple_follow(H, T, sT, record);
+    check2(T, rT);
+    return rT;
 }
 
 auto pt1(auto const& in)
@@ -137,31 +172,63 @@ auto pt1(auto const& in)
         H.second += mv.second;
         T = follow(H, T, sT, true);
     }
-    std::cout << "H (" << H.first << ", " << H.second << ")\n";
-    std::cout << "T (" << T.first << ", " << T.second << ")\n";
+//    std::cout << "H (" << H.first << ", " << H.second << ")\n";
+//    std::cout << "T (" << T.first << ", " << T.second << ")\n";
     return sT.size();
 }
 
+// 2778, too high
 auto pt2(auto const& in)
 {
     std::set<pt_t> sT;
-    std::array<pt_t, 10> H2T {};
-    for(auto& p : H2T)
-        std::cout << p.first << ", " << p.second << "\n";
+    std::array<pt_t, 10> H2T{};
     sT.insert(H2T[9]);
-    for(auto mv : in)
+    for (auto mv : in)
     {
         H2T[0].first += mv.first;
         H2T[0].second += mv.second;
-        for(int n = 1; n < H2T.size(); ++n)
+        for (int n = 1; n < H2T.size(); ++n)
             H2T[n] = follow(H2T[n - 1], H2T[n], sT, n == 9);
-        for(auto& p : H2T)
-            std::cout << "( " << p.first << ", " << p.second << "),";
-        std::cout << "\n";
-//        std::cout << H2T[9].first << ", " << H2T[9].second << "\n";
+        //        for(auto& p : H2T)
+        //            std::cout << "( " << p.first << ", " << p.second << "),";
+        //        std::cout << "\n";
+        //        std::cout << H2T[9].first << ", " << H2T[9].second << "\n";
     }
+    //    for (auto& p : sT)
+    //        std::cout << "( " << p.first << ", " << p.second << "),";
+    //    std::cout << "\n";
+    short xf{ 0 }, xt{ 0 };
+    short yf{ 0 }, yt{ 0 };
+    for (auto& p : sT)
+    {
+        if (p.first < xf)
+            xf = p.first;
+        if (p.first > xt)
+            xt = p.first;
+        if (p.second < yf)
+            yf = p.second;
+        if (p.second > yt)
+            yt = p.second;
+    }
+ #if 0
+    auto cols = xt - xf + 1;
+    auto rows = yt - yf + 1;
+ 
+    std::vector<char> print(rows * cols , '.');
+    for (auto& p : sT)
+    {
+        print[p.first - xf + (p.second - yf) * cols] = '*';
+    }
+    print[-xf + -yf * cols] = 's';
+    auto it = print.begin();
+    for (int n = 0; n < rows; ++n)
+    {
+        std::copy(it, it + cols, std::ostream_iterator<char>(std::cout, ""));
+        std::cout << "\n";
+        it += cols;
+    }
+#endif
     return sT.size();
-
 }
 
 int main()
