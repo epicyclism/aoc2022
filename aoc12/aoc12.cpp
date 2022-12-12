ffc341b029ad9ed58ecaaffda511c59e25f10648
Paul Ranson <paul@epicyclism.com>
2022-12-12 15:25:00 +0000

    aoc12 tidy

--- aoc12/aoc12.cpp
+++ aoc12/aoc12.cpp
@@ -13,11 +13,12 @@ auto get_input()
     size_t stride{0};
     size_t S{0};
     size_t E{0};
-    while(std::getline(std::cin, ln))
+    std::getline(std::cin, ln);
+    stride = ln.size();
+    do
     {
         std::copy(ln.begin(), ln.end(), std::back_inserter(in));
-        stride = ln.size();
-    }
+    } while (std::getline(std::cin, ln));
     for(auto n = 0; n < in.size(); ++n)
     {
         char c;
@@ -40,52 +41,24 @@ auto get_input()
     return std::make_tuple(in, stride, S, E);
 }
 
-// helper for grid
-//
-class grid
-{
-private:
-    const std::vector<char>& data_;
-    const size_t stride_;
-public:
-    grid(std::vector<char> const& d, size_t s) : data_{ d }, stride_{s}
-    {}
-    std::vector<vertex_id_t> operator[](vertex_id_t v) const
-    {
-        auto this_ht { data_[v]};
-        ++this_ht;
-        std::vector<vertex_id_t> rv;
-        // left
-        if (v % stride_ != 0 && (data_[v - 1] <= this_ht))
-            rv.emplace_back(v - 1);
-        // right
-        if ((v % stride_) != (stride_ - 1) && (data_[v + 1] <= this_ht))
-            rv.emplace_back(v + 1);
-        // up
-        if (v > stride_ && (data_[v - stride_] <= this_ht))
-            rv.emplace_back(v - stride_);
-        // down
-        if (v < data_.size() - stride_ && (data_[v + stride_] <= this_ht))
-            rv.emplace_back(v + stride_);
-        return rv;
-    }
-    size_t size() const
-    {
-        return data_.size();
-    }
-};
-
 auto pt1(auto const& in)
 {
-    grid g { std::get<0>(in), std::get<1>(in)};
+    auto vp = [](char f, char t) -> bool { return t <= f + 1;};
+    grid<char, decltype(vp)> g { std::get<0>(in), std::get<1>(in), vp};
     auto d { bfs(g, std::get<2>(in))};
     return d[std::get<3>(in)];
 }
 
 auto pt2(auto const& in)
 {
-    std::cout << std::get<1>(in) << ", " << std::get<2>(in) << ", " << std::get<3>(in) << "\n";
-    return std::get<0>(in).size();
+    auto vp = [](char f, char t) -> bool { return t >= f - 1;};
+    grid<char, decltype(vp)> g { std::get<0>(in), std::get<1>(in), vp};
+    auto d { bfs(g, std::get<3>(in))};
+    auto mp { g.size()};
+    for(int n = 0; n < g.size(); ++n)
+        if(std::get<0>(in)[n] == 0 && d[n] < mp)
+            mp = d[n];
+    return mp;
 }
 
 int main()
