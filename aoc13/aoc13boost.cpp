2ff687afe28b942197444fbfc55ce161f949a855
Paul Ranson <paul@epicyclism.com>
2022-12-14 21:05:31 +0000

    nlohmann json in aoc13

--- aoc13/aoc13boost.cpp
+++ aoc13/aoc13boost.cpp
@@ -1,3 +1,6 @@
+#define BOOST_JSON_HEADER_ONLY
+#define BOOST_JSON_STATIC_LINK
+#include <boost/json.hpp>
 
 #include <utility>
 #include <iostream>
@@ -5,47 +8,68 @@
 #include <vector>
 #include <variant>
 #include <algorithm>
-
-#include <nlohmann/json.hpp>
-using json = nlohmann::json;
+#include <charconv>
 
 auto get_input()
 {
-    std::vector<json> rv;
     std::string ln;
+    std::string lnl;
+    std::string lnr;
+    std::vector<std::pair<std::string, std::string>> in;
     while (std::getline(std::cin, ln))
-        if(!ln.empty())
-            rv.emplace_back(json::parse(ln));
-    return rv;
+    {
+        if(ln.empty())
+		{
+            in.emplace_back(lnl, lnr);
+			lnl.clear();
+		}
+		else
+        if(lnl.empty())
+            lnl = ln;
+        else
+            lnr = ln;
+    }
+    in.emplace_back(lnl, lnr);
+    return in;
 }
 
-bool operator<(json const& l, json const& r)
+bool less_v_v(boost::json::value const& l, boost::json::value const& r)
 {
 	if(l.is_array() && r.is_array())
-		return std::lexicographical_compare(l.begin(), l.end(), r.begin(), r.end());
+		return std::lexicographical_compare(l.as_array().begin(), l.as_array().end(), r.as_array().begin(), r.as_array().end(), less_v_v);
 	if(l.is_array())
-		return l < json::array({r.get<int>()});
+		return less_v_v(l.as_array(), boost::json::value{r.as_int64()});
 	else
 	if(r.is_array())
-		return json::array({l.get<int>()}) < r;
-	return l.get<int>() < r.get<int>();
+		return less_v_v(boost::json::value{l.as_int64()}, r);
+	return l.as_int64 () < r.as_int64();
+}
+
+bool compare(std::pair<std::string, std::string> const& ss)
+{
+#if 1
+	auto vl = boost::json::parse(ss.first);
+	auto vr = boost::json::parse(ss.second);
+	auto b{ less_v_v(vl, vr )};
+	return b;
+#else
+	return false;
+#endif
 }
 
 auto pt1(auto const& in)
 {
     int rv {0};
-    for(int n = 0; n < in.size() / 2; ++n)
-        if(in[2 * n] < in[2 * n + 1])
+    for(int n = 0; n < in.size(); ++n)
+        if( compare(in[n]))
             rv += n + 1;
     return rv;
 }
 
-auto pt2(auto in)
+auto pt2(auto const& in)
 {
-#if 0
     std::string_view p1{ "[[6]]" };
 	std::string_view p2{ "[[2]]" };
-
 	std::vector<std::pair<boost::json::value, int>> vv;
 	vv.emplace_back(boost::json::parse(p1), 0);
 	vv.emplace_back(boost::json::parse(p2), 1);
@@ -58,25 +82,12 @@ auto pt2(auto in)
 	auto pkt1 = std::find_if(vv.begin(), vv.end(), [](auto& p) { return p.second == 0; });
 	auto pkt2 = std::find_if(vv.begin(), vv.end(), [](auto& p) { return p.second == 1; });
 	return (std::distance(vv.begin(), pkt1) + 1) * (std::distance(vv.begin(), pkt2) + 1);
-#else
-    auto jl = json("[[\"2\"]]");
-    auto jr = json("[[\"6\"]]");
-    int cl { 0};
-    int cr { 0};
-    for(auto& v : in)
-    {
-        if( jl < v)
-            ++cl;
-        if(jr < v)
-            ++cr;
-    }
-	return cl*(cr + 1);
-#endif
 }
 
 int main()
 {
-    auto in = get_input();
+	std::cout << "v2\n";
+    auto in {get_input()};
 
     std::cout << "pt1 = " << pt1(in) << "\n";
     std::cout << "pt2 = " << pt2(in) << "\n";
