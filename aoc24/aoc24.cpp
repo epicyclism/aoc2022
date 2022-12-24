c3b96a4349dd08db9527f3dd69b2d31c0379b737
Paul Ranson <paul@epicyclism.com>
2022-12-24 09:30:18 +0000

    update 24 for debug

--- aoc24/aoc24.cpp
+++ aoc24/aoc24.cpp
@@ -1,83 +1,181 @@
 #include <iostream>
+#include <iterator>
 #include <string>
 #include <vector>
+#include <queue>
 #include <algorithm>
-#include <bitset>
 
 #include <experimental/mdspan>
 
 namespace stdex = std::experimental;
 
-constexpr int sz{128};
-
-auto get_input()
-{
-    std::vector<short> arena(sz * sz, 0);
-    stdex::mdspan md(arena.data(), sz, sz);
-
-    int t {sz / 2 + 35};
-    int cnt {0};
-    std::string ln;
-    while(std::getline(std::cin, ln))
-    {
-        int l {sz / 2 - 35};
-        for(auto c : ln)
-        {
-            if(c == '#')
-            {
-                ++cnt; 
-                md(l, t) = cnt;
-            }
-            ++l;
-        }
-        --t;
-    }
-    std::cout << cnt << " elves\n";
-    return arena;
-}
-
-void print(auto const& a)
-{
-    stdex::mdspan md(a.data(), sz, sz);
-    for(auto y = sz - 1; y >= 0; --y)
-    {
-        for(auto x = 0; x < sz; ++x)
-            std::cout << (md(x, y) ? '#' : '.');
-        std::cout << "\n";
-    }
-}
-
-struct pt_t
+struct blizzard_t
 {
     int x_;
     int y_;
+    char d_;
 };
-constexpr pt_t scan[] { {1, 0}, { 1, 1}, {1, -1},  // N NE NW
-                        {-1, 0}, {-1, 1}, {-1, -1},// S SE SW
-                        {0, -1}, {1, -1}, {-1, -1},// W, NW, SW
-                        {0, 1}, {1, 1}, {-1, 1}    // E, NE, SE
-                        };
+
+struct arena_t
+{
+    std::vector<blizzard_t> b_;
+    size_t w_;
+    size_t h_;
+    size_t s_;
+    size_t e_;
+    void step()
+    {
+        for(auto& b: b_)
+        {
+            switch(b.d_)
+            {
+                case '>':
+                    ++b.x_;
+                    if(b.x_ == w_ - 1)
+                        b.x_ = 1;
+                    break;
+                case '<':
+                    --b.x_;
+                    if(b.x_ == 0)
+                        b.x_ = w_ - 2;
+                   break;
+                case '^':
+                    --b.y_;
+                    if( b.y_ == 0)
+                        b.y_ = h_ - 2;
+                    break;
+                case 'v':
+                    ++b.y_;
+                    if(b.y_ == h_ - 1)
+                        b.y_ = 1;
+                    break;
+            }
+        }
+    }
+    std::vector<char> render() const
+    {
+        std::vector<char> a(w_ * h_, '.');
+        std::fill_n(a.begin(), w_, '#');
+        std::fill_n(a.end() - w_, w_, '#');
+        a[s_] = '.';
+        a[e_] = '.';
+        for(int l = 1; l < h_ - 1; ++l)
+        {
+            a[l * w_] = '#';
+            a[l * w_ + w_- 1] = '#';
+        }
+        for(auto const& b: b_)
+            a[b.x_ + w_ * b.y_] = b.d_;
+        return a;
+    }
+};
+
+auto get_input()
+{
+    arena_t a;
+    a.s_ = 1;
+    std::string ln;
+    a.h_ = 0;
+    while (std::getline(std::cin, ln))
+    {
+        a.w_ = ln.size();
+        for(int n = 1; n < a.w_ - 1; ++n)
+            if( ln[n] == '<' || ln[n] == '>' || ln[n] == '^' || ln[n] == 'v')
+                a.b_.emplace_back(n, a.h_, ln[n]);
+        ++a.h_;
+    }
+    a.s_ = 1;
+    a.e_ = a.w_ * a.h_ - 2;
+
+    return a;
+}
+
+void dump(auto const& a)
+{
+    std::cout << "w = " << a.w_ << ", h = " << a.h_ << "\n";
+    std::cout << "s = " << a.s_ << ", e = " << a.e_ << "\n";
+    std::cout << a.b_.size() << " blizzards\n";
+    for(auto& b: a.b_)
+        std::cout << b.x_ << ", " << b.y_ << " : " << b.d_ << "\n";
+}
+
+std::ostream& operator<<( std::ostream& os, arena_t const& a)
+{
+    auto ar { a.render()};
+    int cnt {0};
+    for(auto c: ar)
+    {
+        if(cnt == a.w_)
+        {
+            os << "\n";
+            cnt = 0;
+        }
+        os << c ;
+        ++cnt;
+    }
+    return os;
+}
+
+auto pt0(auto const& in)
+{
+    auto a {in};
+    for(int n = 0; n < 19; ++n)
+    {
+        std::cout << n << "\n" << a << "\n";
+        a.step();
+    }
+    return 0;
+}
+
 auto pt1(auto const& in)
 {
-    auto arena {in};
-    stdex::mdspan md(arena.data(), sz, sz);
-    int scan_base { 0 };
-
-
-
-    return in.size();
+    auto a {in};
+    std::queue<std::pair<int, int>> q; // pos, time
+    int time {0};
+    std::vector<char> ar;
+    q.push({a.s_, 0});
+    while(!q.empty())
+    {
+        auto u = q.front();
+        if(u.second == time)
+        {
+            a.step();
+            ar = a.render();
+            std::cout << time << "\n" << a << "\n";
+            ++time;
+            if( time > 19)
+                break;
+        }
+        std::cout << u.first << ", " << u.second << "\n";
+        q.pop();
+        auto current = u.first;
+        if(current == a.e_)
+            break;
+        if(ar[current] != '.')
+            continue;
+        q.push({current, time}); // stay
+        if(ar[current - 1] == '.')
+            q.push({current - 1, time}); // left
+        if(ar[current + 1] == '.')
+            q.push({current + 1, time}); // right
+        if(ar[current - a.w_] == '.')
+            q.push({current - a.w_, time}); // up
+        if(ar[current + a.w_] == '.')
+            q.push({current + a.w_, time}); // down
+    }
+    return time;
 }
 
 auto pt2(auto const& in)
 {
-    print (in);
-    return in.size();
+    return 0;
 }
 
 int main()
 {
     auto in {get_input()};
-
+//    dump(in);
+//   pt0(in);
     std::cout << "pt1 = " << pt1(in)<< "\n";
     std::cout << "pt2 = " << pt2(in) << "\n";
 }
